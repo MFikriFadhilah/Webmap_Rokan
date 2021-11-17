4090c27cccdbecc69af898bac86576159cfe81ec
MFikriFadhilah <90301397+MFikriFadhilah@users.noreply.github.com>
2021-11-17 16:03:47 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 // Where to load data from. Choose either a local file in GitHub repo, such as `data/places.csv`,
 // or a Google Sheets URL (File > Publish to the Web > CSV )
-const dataLocation = 'data/places.csv';
+const dataLocation = 'data/KPH_Rokan.csv';
 
 // Map's initial center, and zoom level
-const mapCenter = [0.6795, 101.345];
+const mapCenter = [0.649844, 117.948];
 const mapZoom = 12;
 
 // Marker icon height and width
