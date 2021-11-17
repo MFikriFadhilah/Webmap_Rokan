8b291b44d378768cc1dbf8f21af8355d14078fe6
MFikriFadhilah <90301397+MFikriFadhilah@users.noreply.github.com>
2021-11-17 18:47:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -375,45 +375,6 @@ var initMap = function() {
       interactive: true,
     }
   }
-//style when hovered
-function highlightFeature(e) {
-   var layer = e.target;
-
-   layer.setStyle({
-      opacity: 1,
-      color: 'rgba(35,35,35,1.0)',
-      dashArray: '',
-      lineCap: 'butt',
-      lineJoin: 'miter',
-      weight: 3.0,
-      fill: true,
-      fillOpacity: 1,
-      fillColor: 'rgba(135,116,158,0.3)',
-      interactive: true,
-   });
-
-   if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-      layer.bringToFront();
-   }
-   info.update(layer.feature.properties); 
-}
-//reset hover state
-function resetHighlight(e) {
-   layer_BatasKawasan.resetStyle(e.target);
-   info.update(); 
-}
-//zoom while clicking
-function zoomToFeature(e) {
-   map.fitBounds(e.target.getBounds());
-}
-// event listener package
-function onEachFeature(feature, layer) {
-   layer.on({
-      mouseover: highlightFeature,
-      mouseout: resetHighlight,
-      click: zoomToFeature
-   });
-}
 
   map.createPane('pane_PohonAdopsiMinastahura_4');
   map.getPane('pane_PohonAdopsiMinastahura_4').style.zIndex = 1;
@@ -429,22 +390,6 @@ function onEachFeature(feature, layer) {
     //pane: 'pane_PohonAdopsiMinastahura_4'
   });
   map.addLayer(layer_BatasKawasan);
-
-//Get "kabupat" from Geojsonfile for area information when hovered
-var info = L.control();
-info.onAdd = function (map) {
-   this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-   this.update();
-   return this._div;
-};
-
-// method that we will use to update the control based on feature properties passed
-info.update = function (props) {
-   this._div.innerHTML = '<h4>Daerah</h4>' + (props ?
-      props.kabupat
-      : '');
-};
-info.addTo(map);
 
   //group basemaps
   basemaps= {
